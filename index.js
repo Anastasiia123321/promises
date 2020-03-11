ef1183fd95241040d4337378d707bfada2f77489
Anastasiia <anastasiya123@meta.ua>
2020-03-11 23:10:23 +0200

    hw#11

---
+++ index.js
@@ -0,0 +1,28 @@
+ const getRandomChinese = async (length) => {
+        let result = '';
+      
+        for (let i = 0; i < length; i++){
+          const randomSign = new Promise(resolve => {
+            setTimeout(() => {
+              let sign = String(Date.now())
+              sign = sign.substring(sign.length - 4)
+              resolve(String.fromCharCode(sign));
+            }, 50);
+          });
+      
+          await randomSign.then((sign) => {
+            result += sign
+          })
+        }
+      
+        console.log(result)
+        return result
+      }
+      
+      
+      const startConvertation = async () => {
+        const anotherResult = document.querySelector('.result');
+        const length = Number(prompt('Введіть число', 4)) 
+        
+        anotherResult.innerHTML += `Result  => ${await getRandomChinese(length)}`
+      }
