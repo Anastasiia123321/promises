3eb9e1287fd7991b29cd4182d257d424c78b6d80
Anastasiia123321 <60201048+Anastasiia123321@users.noreply.github.com>
2020-03-17 23:18:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const getRandomChinese = async (length) => {
-        let result = '';
-      
-        for (let i = 0; i < length; i++){
-          const randomSign = new Promise(resolve => {
-            setTimeout(() => {
-              let sign = String(Date.now())
-              sign = sign.substring(sign.length - 4)
-              resolve(String.fromCharCode(sign));
-            }, 50);
-          });
-      
-          await randomSign.then((sign) => {
-            result += sign
-          })
-        }
-        return result;
-      }
-      
-      
-      const startConvertation = async () => {
-        const anotherResult = document.querySelector('.result');
-        const length = Number(prompt('Введіть число', 4));
-        
-        anotherResult.innerHTML += `Result  => ${await getRandomChinese(length)}
-      }
+  let result = '';
+
+  for (let i = 0; i < length; i++){
+    const randomSign = new Promise(resolve => {
+      setTimeout(() => {
+        let sign = String(Date.now())
+        sign = sign.substring(sign.length - 5)
+        resolve(String.fromCharCode(sign));
+      }, 50);
+    });
+
+    await randomSign.then((symbol) => {
+      result += symbol;
+    })
+  }
+
+  return result;
+}
+
+
+const beginGetRandomChinese = async () => {
+  const ress = document.querySelector('.result');
+  const length = Number(prompt('Enter number', 5)) 
+  
+  ress.innerHTML += `Result  => ${await getRandomChinese(length)}`
+}
